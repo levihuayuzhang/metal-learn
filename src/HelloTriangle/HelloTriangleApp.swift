0aae954b71b3ad96e32c2011f0eefc5594d041ad
Levi Zhang <zhanghuayu.dev@gmail.com>
2022-12-31 02:04:12 +0000

    hello triangle

---
+++ src/HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by zhy on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 800, height: 600)
+        }
+    }
+}
+
---
+++ src/HelloTriangle/definitions.h
@@ -0,0 +1,18 @@
+//
+//  definitions.h
+//  HelloTriangle
+//
+//  Created by zhy on 12/30/22.
+//
+
+#ifndef definitions_h
+#define definitions_h
+
+#include <simd/simd.h>
+
+struct Vertex {
+    vector_float2 position;
+    vector_float4 color;
+};
+
+#endif
